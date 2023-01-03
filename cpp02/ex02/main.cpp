e82a303d55b000d1ddbe94395a2201ba50dab13e
Cicek Yuzbas <cicekyuzbas@ciceks-imac.kpn>
2023-01-03 23:09:27 +0100

    ex02 done

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cyuzbas <cyuzbas@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/30 20:07:05 by cyuzbas       #+#    #+#                 */
-/*   Updated: 2023/01/03 23:07:31 by cyuzbas       ########   odam.nl         */
+/*   Updated: 2023/01/03 23:09:17 by cyuzbas       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int main(void)
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
-
 	
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;

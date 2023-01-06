9309d4e3841876eb8a73937bf1927856ca0b3698
Cicek Yuzbas <cicekyuzbas@ciceks-imac.kpn>
2023-01-06 17:29:54 +0100

    better test in main

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: cyuzbas <cyuzbas@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/04 14:00:28 by cyuzbas       #+#    #+#                 */
-/*   Updated: 2023/01/04 22:26:03 by cyuzbas       ########   odam.nl         */
+/*   Updated: 2023/01/06 17:21:33 by cyuzbas       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 ClapTrap::ClapTrap( void ) : _name("default"), \
 	_hitPoints(10), _energyPoints(10), _attackDamage(0){
-	std::cout << GRAY << "Default constructor called" << RESET << std::endl;
+	std::cout << GRAY << "Default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) : _name(name), \
 	_hitPoints(10), _energyPoints(10), _attackDamage(0){
-	std::cout << GRAY << "String constructor called" << RESET << std::endl;
+	std::cout << GRAY << "String constructor called" << std::endl;
 }
 
 ClapTrap::~ClapTrap( void ){
-	std::cout << GRAY << "Destructor called" << RESET << std::endl;
+	std::cout << GRAY << "Destructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap( ClapTrap const & src ){
-	std::cout << GRAY << "Copy Constructor called" << RESET << std::endl;
+	std::cout << GRAY << "Copy Constructor called" << std::endl;
 	*this = src;
 }
 
@@ -43,35 +43,35 @@ ClapTrap &ClapTrap::operator=( ClapTrap const &src ){
 void ClapTrap::attack(const std::string& target){
 	if (this->_hitPoints && this->_energyPoints){
 		this->_energyPoints--;
-		std::cout << GREEN << "ClapTrap " << this->_name << " attacks " <<
-		target << ", causing '" << this->_attackDamage << "' points of damage!"
-		<< " (LEFT EP: "<< this->_energyPoints<< ")" << RESET << std::endl;
+		std::cout << GREEN << "ClapTrap " << this->_name
+		<< " attacks " << target << ", causing '" << this->_attackDamage
+		<< "' points of damage!" << " (LEFT HP: "<< this->_hitPoints << ")"
+		<< " (LEFT EP: "<< this->_energyPoints<< ")" << std::endl;
 	} else {
-		std::cout << YELLOW << "WARNING! ClapTrap " << 
-		this->_name << " is unable to attack due to";
+		std::cout << YELLOW << "WARNING! ClapTrap "
+		<< this->_name << " is unable to attack due to";
 		if (!this->_energyPoints)
-			std::cout << " insufficent energy points!!" << RESET << std::endl;
+			std::cout << " insufficent energy points!!" << std::endl;
 		else
-			std::cout << " his DEATH!!" << RESET << std::endl;
+			std::cout << " his DEATH!!" << std::endl;
 	}
 }
 
 void ClapTrap::takeDamage(unsigned int amount){
 	if (this->_hitPoints && this->_energyPoints){
 		if (amount >= this->_hitPoints)
-			this->_hitPoints = 0;
-		else
-			this->_hitPoints -= amount;
+			amount = 10;
+		this->_hitPoints -= amount;
 		std::cout << RED << "ClapTrap " << this->_name << " got attacked, " << 
 		"and lost '" << amount << "' hit points! (LEFT HP: " << this->_hitPoints
-		<< ")" << RESET << std::endl;
+		<< ")" << " (LEFT EP: " << this->_energyPoints << ")" << std::endl;
 	} else {
 		std::cout << YELLOW << "WARNING! ClapTrap " << this->_name << 
 		" is unable to get attacked due to ";
 		if (!this->_energyPoints)
-			std::cout << "insufficent energy points!!" << RESET << std::endl;
+			std::cout << "insufficent energy points!!" <<  std::endl;
 		else
-			std::cout << "his DEATH!!" << RESET << std::endl;
+			std::cout << "his DEATH!!" << std::endl;
 	}
 }
 
@@ -81,14 +81,14 @@ void ClapTrap::beRepaired(unsigned int amount){
 		this->_energyPoints--;
 		std::cout << BLUE << "ClapTrap " << this->_name 
 		<< " found a healing potion and gained '" << amount
-		<< "' hit points back! (LEFT HP: " << this->_hitPoints
-		<< " )" << RESET << std::endl;
+		<< "' hit points! (LEFT HP: " << this->_hitPoints << ")" 
+		<< " (LEFT EP: " << this->_energyPoints << " )" << std::endl;
 	} else {
 		std::cout << YELLOW << "WARNING! ClapTrap " << 
 		this->_name << " is unable to heal due to ";
 		if (!this->_energyPoints)
-			std::cout << "insufficent energy points!!" << RESET << std::endl;
+			std::cout << "insufficent energy points!!"  << std::endl;
 		else
-			std::cout << "his DEATH!!" << RESET << std::endl;
+			std::cout << "his DEATH!!"  << std::endl;
 	}
 }
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,27 +6,58 @@
 /*   By: cyuzbas <cyuzbas@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/04 14:00:22 by cyuzbas       #+#    #+#                 */
-/*   Updated: 2023/01/04 22:30:55 by cyuzbas       ########   odam.nl         */
+/*   Updated: 2023/01/06 17:26:55 by cyuzbas       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int main (void)
+int main(int argc, char **argv) {
+  (void)argv;
+  if (argc != 1)
+    exit(EXIT_FAILURE);
 {
-	ClapTrap a;
-	ClapTrap b("R2-D2");
+	std::cout << std::endl;
+	std::cout << PINK << "[ TEST_ClapTrap : Default constructor & Destructor ]" << std::endl;
+	ClapTrap robot;
+}
+{
+	std::cout << std::endl;
+	std::cout << "[ TEST_ClapTrap : Name constructor & Destructor ]" << std::endl;
+	ClapTrap robot("Clap");
+}
+{
+	std::cout << std::endl;
+	std::cout << PINK << "[ TEST_ClapTrap : Default constructor & Copy constructor & Copy assignment operator & Destructor ]" << std::endl;
+	ClapTrap robot1;
+	ClapTrap robot(robot1);
+}
+{
+	std::cout << std::endl;
+	std::cout << PINK << "[ TEST_ClapTrap : Attack & Take damage & Be repaired x1 ]" << std::endl;
+	ClapTrap robot1("Clap");
+	ClapTrap robot2("Trap");
+	robot1.attack("Trap");
+	robot2.takeDamage(0);
+	robot2.beRepaired(0);
+}
+{
+	std::cout << std::endl;
+	std::cout << PINK << "[ TEST_ClapTrap : Attack & Take damage & Die & Be repaired & Finished Energy ]" << std::endl;
+	ClapTrap robot1;
+	ClapTrap robot2("R2-D2");
 
-	a.attack("C-3PO");
-	a.takeDamage(15);
-	a.takeDamage(10);
-	a.beRepaired(5);
-	a.attack("Some Droid");
-	b.beRepaired(3);
-	b.beRepaired(5);
+	robot1.attack("C-3PO");
+	robot1.takeDamage(15);
+	robot1.takeDamage(10);
+	robot1.beRepaired(5);
+	robot1.attack("Some Droid");
+	robot2.beRepaired(3);
+	robot2.beRepaired(5);
 	for (int i = 0; i < 10; i++)
-		b.attack("BB-8");
-	b.beRepaired(3);
-
-	return 0;
+		robot2.attack("BB-8");
+	robot2.beRepaired(3);
+}
+  std::cout << std::endl;
+  return (0);
 }

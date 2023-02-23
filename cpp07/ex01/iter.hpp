99503354f9625326e8f5b051292c58320e945a7e
Cicek Yuzbas <cicekyuzbas@ciceks-imac.kpn>
2023-02-23 07:33:56 +0100

    ex01 strted

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   iter.hpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cyuzbas <cyuzbas@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/20 22:30:21 by cyuzbas       #+#    #+#                 */
+/*   Updated: 2023/02/20 22:33:30 by cyuzbas       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template<typename T>
+void	iter(T *array, T len, T );
+
+#endif
